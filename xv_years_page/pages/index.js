e5d24cc3dad50e8821aa3899d4c1122e5a8a0a12
Vector4c5 <hernandez435627@gmail.com>
2026-02-22 01:12:32 -0600

    Se agrega formato para celulares

--- xv_years_page/pages/index.js
+++ xv_years_page/pages/index.js
@@ -11,7 +11,7 @@ const tangerine = Tangerine({
 export default function Home() {
   return (
     <div
-      className="flex min-h-screen w-full flex-col items-center justify-center bg-vic-6"
+      className="flex h-screen w-full flex-col items-center justify-center bg-vic-6"
     >
       <StarAnimation />
       <div className="fixed inset-0 z-0 w-full h-full">
@@ -31,29 +31,31 @@ export default function Home() {
         <div className="w-full h-full z-10 inset-0 flex items-center justify-start flex-col p-4 sm:p-6 md:p-10">
           <div className="flex flex-col w-full items-center gap-3 justify-center">
             <div>
-              <h1 className={`text-3xl sm:text-4xl md:text-5xl lg:text-7xl text-center font-bold text-vic-1 ${tangerine.className}`}>
+              <h1 className={`text-5xl sm:text-4xl md:text-5xl lg:text-7xl text-center font-bold text-vic-1 ${tangerine.className}`}>
                 Acompáñame en este día tan especial
               </h1>
             </div>
-            <div className="w-40 h-40 sm:w-48 sm:h-48 md:w-56 md:h-56 lg:w-64 lg:h-64 border-2 sm:border-3 md:border-4 border-vic-5 overflow-hidden bg-vic-1">
+            <div className="w-3/4 h-auto sm:w-48 sm:h-48 md:w-56 md:h-56 lg:w-64 lg:h-auto border-2 sm:border-3 md:border-4 border-vic-5 overflow-hidden bg-vic-1">
               <img
                 src="/Foto_2.png"
-                alt="Logo"
+                alt="Foto_De_XV"
                 className="w-full h-full object-cover"
               />
             </div>
             <div
-              className="flex flex-col items-center justify-start h-auto w-full sm:w-3/4">
-              <p className={`text-vic-1 text-bold text-lg sm:text-xl md:text-3xl lg:text-5xl text-center ${tangerine.className}`}>
+              className="flex flex-col items-center justify-start h-auto w-full sm:w-3/4 gap-2">
+              <p className={`text-vic-1 text-bold text-3xl sm:text-xl md:text-3xl lg:text-5xl text-center border-b-2 border-vic-4
+                 ${tangerine.className}`}>
                 Aylin Maritza Hernandez Maldonado
               </p>
-              <p className={`text-vic-1 text-lg sm:text-xl md:text-3xl lg:text-5xl text-bold text-center ${tangerine.className}`}>
+              <p className={`text-vic-1 text-3xl sm:text-xl md:text-3xl lg:text-5xl text-bold text-center ${tangerine.className}`}>
                 Celebremos juntos este momento inolvidable
               </p>
             </div>
             <Link
               href="/invitacion"
-              className={`px-4 py-2 sm:px-5 sm:py-2.5 md:px-6 md:py-3 bg-vic-3 text-white text-sm sm:text-base md:text-lg lg:text-2xl rounded-lg sm:rounded-xl mt-2 sm:mt-3 md:mt-4 ${tangerine.className} border border-vic-1 sm:border-2
+              className={`px-5 py-1 sm:px-5 sm:py-2.5 md:px-6 md:py-3 bg-vic-3 text-white text-2xl sm:text-base md:text-lg lg:text-2xl 
+                rounded-lg sm:rounded-xl mt-2 sm:mt-3 md:mt-4 ${tangerine.className} border border-vic-1 sm:border-2
                 transform transition-all duration-300 ease-out hover:scale-110 hover:bg-vic-4 hover:text-black`}
             >
               Abrir Invitación
